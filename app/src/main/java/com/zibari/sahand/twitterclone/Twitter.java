8a8df3edb14d939a34b6b87728193f546129ad46
smz1989 <sahandmohammadiziabari@gmail.com>
2019-02-23 16:09:34 +0100

    Adding MenuBar

--- app/src/main/java/com/zibari/sahand/twitterclone/Twitter.java
+++ app/src/main/java/com/zibari/sahand/twitterclone/Twitter.java
@@ -2,6 +2,10 @@ package com.zibari.sahand.twitterclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class Twitter extends AppCompatActivity {
 
@@ -10,4 +14,32 @@ public class Twitter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter);
     }
+
+    // Transition to Twitter.Java file // end
+
+    // creating menu on top of the folder //start  (Adding Menu- Sharing Images)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+
+        // creating menu on top of the folder //End  (Adding Menu- Sharing Images)
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.logoutUserItem){
+
+            ParseUser.getCurrentUser().logOut();
+            finish();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
