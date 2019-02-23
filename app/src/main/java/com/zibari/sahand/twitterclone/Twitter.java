3f2226030be39c928a4e06851a5ec1408a5e929a
smz1989 <sahandmohammadiziabari@gmail.com>
2019-02-23 19:12:55 +0100

    Adding Simple ListView  && ParseQuery

--- app/src/main/java/com/zibari/sahand/twitterclone/Twitter.java
+++ app/src/main/java/com/zibari/sahand/twitterclone/Twitter.java
@@ -1,19 +1,78 @@
 package com.zibari.sahand.twitterclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Twitter extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<String> tUsers;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter);
+
+
+        // Initializing Users in listview  /  ParseQuery  /// Start
+
+
+
+        FancyToast.makeText(this, "Welcome" + ParseUser.getCurrentUser().getUsername(), FancyToast.INFO, Toast.LENGTH_LONG, true).show();
+        listView = findViewById(R.id.listView);
+        tUsers = new ArrayList<>();
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tUsers);
+
+        try {
+
+            ParseQuery<ParseUser> query = ParseUser.getQuery();
+            query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+            query.findInBackground(new FindCallback<ParseUser>() {
+                @Override
+                public void done(List<ParseUser> objects, ParseException e) {
+
+                    if (objects.size() > 0 && e == null){
+
+                        for (ParseUser twitterUser : objects){
+
+                            tUsers.add(twitterUser.getUsername());
+                        }
+
+                        listView.setAdapter(adapter);
+                    }
+
+                }
+            });
+
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+
+
     }
+
+    // Initializing Users in listview  /  ParseQuery  /// END
+
 
     // Transition to Twitter.Java file // end
 
@@ -33,12 +92,25 @@ public class Twitter extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId() == R.id.logoutUserItem){
 
-            ParseUser.getCurrentUser().logOut();
-            finish();
+        switch (item.getItemId()){
 
+            case R.id.logoutUserItem:
+
+                ParseUser.getCurrentUser().logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        Intent intent = new Intent(Twitter.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                });
+                break;
         }
+
+
         return super.onOptionsItemSelected(item);
     }
 
