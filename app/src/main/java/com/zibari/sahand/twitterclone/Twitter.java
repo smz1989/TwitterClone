7120646c1d51698dfd1fc3e409a2d1aa6de2173c
smz1989 <sahandmohammadiziabari@gmail.com>
2019-02-24 06:34:52 +0100

    Adding Following and Unfollowing Users

--- app/src/main/java/com/zibari/sahand/twitterclone/Twitter.java
+++ app/src/main/java/com/zibari/sahand/twitterclone/Twitter.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -14,21 +18,28 @@ import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Twitter extends AppCompatActivity {
+public class Twitter extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
     private ListView listView;
+    // For keeping the users inside
     private ArrayList<String> tUsers;
+
     private ArrayAdapter adapter;
+    private String followedUser = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter);
+
+
+
 
 
         // Initializing Users in listview  /  ParseQuery  /// Start
@@ -38,7 +49,13 @@ public class Twitter extends AppCompatActivity {
         FancyToast.makeText(this, "Welcome" + ParseUser.getCurrentUser().getUsername(), FancyToast.INFO, Toast.LENGTH_LONG, true).show();
         listView = findViewById(R.id.listView);
         tUsers = new ArrayList<>();
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tUsers);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, tUsers);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+
+        // For checking or unChecking other users Initialization // Start 1
+        listView.setOnItemClickListener(Twitter.this);
+        // For checking or unChecking other users // end 1
+
 
         try {
 
@@ -56,6 +73,27 @@ public class Twitter extends AppCompatActivity {
                         }
 
                         listView.setAdapter(adapter);
+
+                            // Solving adding users twice when run the apps again /// Start
+
+                        for (String twitterUsers:tUsers){
+
+                            if (ParseUser.getCurrentUser().getList("fanOf") != null) {
+
+                                if (ParseUser.getCurrentUser().getList("fanOf").contains(twitterUsers)) {
+
+                                    followedUser = followedUser + twitterUsers + "\n";
+
+                                    listView.setItemChecked(tUsers.indexOf(twitterUsers), true);
+
+                                    FancyToast.makeText(Twitter.this,ParseUser.getCurrentUser().getUsername() + " is following " + followedUser, Toast.LENGTH_SHORT, FancyToast.INFO, true).show();
+                                }
+                            }
+
+
+                        }
+
+                           // Solving adding users twice when run the apps again /// End
                     }
 
                 }
@@ -114,4 +152,49 @@ public class Twitter extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    // For checking or unChecking other users Initialization // Start 2
+
+    @Override
+    public void onItemClick(AdapterView<?> parent,
+                            View view, int position, long id) {
+
+        CheckedTextView checkedTextView = (CheckedTextView) view;
+
+        if (checkedTextView.isChecked()) {
+            FancyToast.makeText(Twitter.this, tUsers.get(position) + " is now followed", Toast.LENGTH_SHORT, FancyToast.INFO, true).show();
+
+            ParseUser.getCurrentUser().add("fanOf", tUsers.get(position));
+
+        }else{
+
+                              // Unfollowing Users  // Start
+
+            FancyToast.makeText(Twitter.this, tUsers.get(position) + " is now unfollowed", Toast.LENGTH_SHORT, FancyToast.INFO, true).show();
+
+            ParseUser.getCurrentUser().getList("fanOf").remove(tUsers.get(position));
+            List currentUsersFanOfList = ParseUser.getCurrentUser().getList("fanOf");
+            ParseUser.getCurrentUser().remove("fanOf");
+            ParseUser.getCurrentUser().put("fanOf", currentUsersFanOfList);
+
+                            // Unfollowing Users  // End
+
+        }
+
+        ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+
+                if (e == null){
+
+                    FancyToast.makeText(Twitter.this, " Saved", Toast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+
+                }
+
+            }
+        });
+
+
+    }
+
+    // For checking or unChecking other users Initialization // end 2
 }
